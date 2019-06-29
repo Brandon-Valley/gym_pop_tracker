d97a5ccfd41980c8aa2b951fe823db93bebceb6e
Brandon Valley <bavvh8@mst.edu>
2019-06-29 10:16:50 -0500

    cleaned up a bit

--- dl_data.py
+++ dl_data.py
@@ -5,7 +5,7 @@ import io
 
 
 
-def download_google_sheet_as_csv(shareable_link_id, output_csv_path):
+def download_google_sheet_as_csv(shareable_link, output_csv_path):
     
     headers={}
     headers["User-Agent"]= "Mozilla/5.0 (Windows NT 6.2; WOW64; rv:22.0) Gecko/20100101 Firefox/22.0"
@@ -15,13 +15,11 @@ def download_google_sheet_as_csv(shareable_link_id, output_csv_path):
     headers["Accept-Language"]= "en-US,en;q=0.5"
     lines = []
     
-    file_id=shareable_link_id
+    file_id = shareable_link.split('=')[-1]
+    
     url = "https://docs.google.com/spreadsheets/d/{0}/export?format=csv".format(file_id)
     
     r = requests.get(url)
-    
-    data = {}
-    cols = []
     
     sio = io.StringIO( r.text, newline=None)
     reader = csv.reader(sio, dialect=csv.excel)
@@ -29,18 +27,7 @@ def download_google_sheet_as_csv(shareable_link_id, output_csv_path):
     
     lines = []
     for row in reader:
-        print(row)
         lines.append(row)
-        if rownum == 0:
-            for col in row:
-                data[col] = ''
-                cols.append(col)
-        else:
-            i = 0
-            for col in row:
-                data[cols[i]] = col
-                i = i +1
-        rownum = rownum + 1
         
     with open(output_csv_path, 'wt', encoding='utf8') as writeFile:
         writer = csv.writer(writeFile, lineterminator = '\n')
@@ -48,5 +35,6 @@ def download_google_sheet_as_csv(shareable_link_id, output_csv_path):
         
     writeFile.close()
     
-#     https://drive.google.com/open?id=1o3qlhQPgk_VM2i48y2DPBhsI6fGFeCtsXyTmU31RWAs
-download_google_sheet_as_csv("1o3qlhQPgk_VM2i48y2DPBhsI6fGFeCtsXyTmU31RWAs", 'gym_pop.csv')
+if __name__ == '__main__':
+    download_google_sheet_as_csv("https://drive.google.com/open?id=1o3qlhQPgk_VM2i48y2DPBhsI6fGFeCtsXyTmU31RWAs", 'gym_pop.csv')
+    print('done')
